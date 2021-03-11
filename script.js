73c69dc3e09269a9cb2b12cc4935ab5942eeb569
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-03-11 09:49:24 +0700

    add again button

--- script.js
+++ script.js
@@ -1,42 +1,64 @@
-// let secretNumber = Math.trunc(Math.random() * 20 + 1);
-let secretNumber = 1;
+let secretNumber = Math.trunc(Math.random()*20 + 1);
 let score = 20;
-let highscore = 0;
+let highScore = 0;
 
-const displayMessage = function (massage) {
-  document.querySelector(".message").textContent = massage;
+// Display message
+const displayMessage = message => {
+  document.querySelector(".message").textContent = message;
 };
 
-// Check Button
-document.querySelector(".check").addEventListener("click", function () {
+// Checking Button
+document.querySelector(".check").addEventListener("click", () => {
   const guess = Number(document.querySelector(".guess").value);
 
-  // Check guess is null or not
+  // Handling invalid input
   if (!guess || isNaN(guess)) {
-    displayMessage("⛔️ No number!");
-  }
-
-  // When guess is Correct
-  else if (guess === secretNumber) {
+    displayMessage("Invalid number 🚫 🚫  🚫");
+  } else if (guess === secretNumber) {
+    // Correct answer
     displayMessage("🎉 Correct Number!");
-
     document.querySelector(".number").textContent = secretNumber;
+
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
   }
 
-  if (score > highscore) {
-    highscore = score;
-    document.querySelector(".highscore").textContent = highscore;
+  // Handling False guess and out of score
+  if (score > highScore) {
+    highScore = score;
+    document.querySelector(".highscore").textContent = highScore;
+    displayMessage("Incorrect !!!")
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      displayMessage(guess >= secretNumber ? "📈 Too high!" : "📉 Too low!");
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥 You lost the game!");
+      document.querySelector(".score").textContent = 0;
     }
-    score--;
-    document.querySelector(".score").textContent = score;
-  } else {
-    // document.querySelector('.message').textContent = '💥 You lost the game!';
-    displayMessage("💥 You lost the game!");
-    document.querySelector(".score").textContent = 0;
   }
 });
+
+
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+
+
+
+// Again Button
+// document.querySelector(".again").addEventListener("click", ()=>{
+
+// })
