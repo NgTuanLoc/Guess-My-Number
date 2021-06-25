975a889d50adf15804ae86c88239e9489778b7d9
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-06-25 15:24:54 +0700

    display movement manipulate username computing balance

--- bankist/script.js
+++ bankist/script.js
@@ -61,16 +61,39 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = movements => {
+  containerMovements.innerHTML = '';
+  movements.forEach((movement, index) => {
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">
+          ${index + 1} ${type}
+        </div>
+        <div class="movements__value">${movement}</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+const calcDisplayBalance = (movements)=>{
+  const balance = movements.reduce((acc, mov)=>{
+    return acc+mov
+  }, 0)
+  labelBalance.textContent = `${balance} EUR`
+}
+calcDisplayBalance(account1.movements)
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
+const arr = [1, 312312 ,13, 34, 123]
+console.log(arr.reduce((acc, curr)=>{
+  if(acc>curr){
+    return acc
+  } else{
+    return curr
+  }
+}));
