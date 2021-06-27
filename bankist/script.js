d00807c2a7eefbd9b6d07f45d3e215aaa48066cb
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-06-27 20:58:53 +0700

    add close account

--- bankist/script.js
+++ bankist/script.js
@@ -173,3 +173,18 @@ btnTransfer.addEventListener('click', event => {
     updateUI(currentAccount);
   }
 });
+
+btnClose.addEventListener('click', event => {
+  event.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(acc => {
+      return acc.username === currentAccount.username;
+    });
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
